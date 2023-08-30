5c3c58f97935dc35b1c774f2b8c7900064741aec
RaoniSilvestre <raoni.silvestre@hotmail.com>
2023-08-30 16:51:10 -0300

    miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 //import styled from "styled-components";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { TransactionsTable } from "./components/TransactionsTable";
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global";
+import { createServer } from 'miragejs';
 
 function App() {
+
+  createServer({
+    routes() {
+      this.namespace = 'api';
+      this.get('/transactions', () => {
+        return [
+          {
+            id: 1,
+            title: 'transaction 1',
+            amount: 400,
+            type: 'deposit',
+            category: 'Food',
+            createdAt: new Date()
+          }
+        ]
+      })
+    },
+  })
   return (
     <>
       <Header />
       <Dashboard />
-      <TransactionsTable/>
       <GlobalStyle />
     </>
   );
