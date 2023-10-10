a041070f088e68661b611d67a0b9d0605f212a48
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-09 21:50:49 -0300

    creating modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,24 +1,57 @@
-
-import Modal from "react-modal"
-import { Container } from "./styled";
-
-interface ModalPopUpProps{
-
-    isOpen: boolean;
-
+import Modal from "react-modal";
+import { Container, TransactionTypeContainer } from "./styled";
+import incomeImg from "./../../assets/Entradas.svg"
+import outcomeImg from "./../../assets/Saídas.svg"
+import closeImg from "./../../assets/Fechar.svg" 
+import { useState } from "react";
+interface ModalPopUpProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
 }
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
-export function ModalPopUp({isOpen}:ModalPopUpProps) {
+export function ModalPopUp({ isOpen, onRequestClose }: ModalPopUpProps) {
 
-    return(
-        <Modal
-        isOpen={isOpen}
-        >
-            <Container>
-            <h1>oii haha</h1>
-            </Container>
-        </Modal>
-    )
+  const [type, setType] = useState('deposit')
+
+  return (
+    <Modal 
+    isOpen={isOpen} 
+    onRequestClose={onRequestClose} 
+    className={"react-modal-content"}
+    overlayClassName={"react-modal-overlay"}
+    >
+      <button type="button" onClick={onRequestClose} className="react-modal-close"><img src={closeImg} alt="fechar modal" /></button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título"/>
+
+        <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <button
+          type="button"
+          onClick={()=>{ setType('deposit')}}
+           
+          >
+            <img src={incomeImg} alt="income" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+          type="button"
+          onClick={()=>{ setType('withdraw')}}
+          >
+            <img src={outcomeImg} alt="outcome" />
+            <span>Saída</span>
+          </button>
+
+        </TransactionTypeContainer>
+        <input type="text" placeholder="Categoria"/>
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
 }
