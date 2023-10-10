357cc29d2f7dd8c490501bb40f10f439eff7a297
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-09 21:51:18 -0300

    creating dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,11 +2,11 @@ import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
-export function Dashboard(){
-    return (
-        <Container>
-            <Summary/>
-            <TransactionsTable/>
-        </Container>
-    )
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
 }
