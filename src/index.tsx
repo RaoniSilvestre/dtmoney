4463b9a169ebb775c58a1daf7e44310f3028edf0
Raoni <raoni.silvestre@hotmail.com>
2023-12-20 11:48:00 -0300

    creating virtual db with axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createServer } from 'miragejs';
+import { Model, createServer } from 'miragejs';
 
 createServer({
-    routes() {
-      this.namespace = 'api';
-      this.get('/transactions', () => {
-        return [
-          {
-            id: 1,
-            title: 'transaction 1',
-            amount: 400,
-            type: 'deposit',
-            category: 'Food',
-            createdAt: new Date()
-          }
-        ]
-      })
-    },
-  })
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Monitoria',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          value: 700,
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Hamburgão',
+          type: 'withdraw',
+          category: 'Alimentação',
+          value: 30,
+          createdAt: new Date()
+        },
+        {
+          id: 3,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          value: 600,
+          createdAt: new Date()
+        }
+      ]
+    })
+  },
+
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
+    })
+  },
+
+
+})
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
   <React.StrictMode>
     <App />
