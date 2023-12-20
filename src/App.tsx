7515ef71151df6cda4bbbd9ab2d43b5f02a86b0b
Raoni <raoni.silvestre@hotmail.com>
2023-12-20 10:06:59 -0300

    Add Modal component and set app element for accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //import styled from "styled-components";
 import { useState } from "react";
+import Modal from "react-modal";
 import { ModalPopUp } from "./components/Modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
-
+Modal.setAppElement("#root"); //Acessibilidade
 
 function App() {
 
